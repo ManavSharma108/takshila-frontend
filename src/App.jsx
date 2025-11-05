062f046fa20a6d01bf27403bb8186898ad93984c
ManavSharma108 <manavsharma@takshila.co>
2025-11-05 18:56:39 +0530

    small chadas

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,9 @@ import OurStory from "./pages/OurStory";
 import Blogs from "./pages/Blogs";
 import axios from "axios"
 export default function App() {
-  const check=async(e)=>{
-    try {
-      console.log("in")
-      axios.defaults.withCredentials=true
-      const {data}=axios.get(`${import.meta.env.VITE_BASE_URL}/api/auth/check`);
-      console.log(data)
-    } catch (error) {
-      console.log(error)
-    }
-  }
+
   return (
     <div className="bg-[#111] text-white min-h-screen">
-      <button className="cursor-pointer z-100 p-4 bg-blue-500" onClick={check}>CHECK AUTH</button>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
